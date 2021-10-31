650c72ca383d3e3e2ddf33f6d9938c1c6c78f651
chevi42 <chevih42@gmail.com>
2021-10-31 17:54:48 +0700

    update

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,38 +1,30 @@
-<body>
-    <div class="wrapper d-flex align-items-stretch">
-      <nav id="sidebar">
-        <div class="custom-menu">
-          <button type="button" id="sidebarCollapse" class="btn btn-primary"></button>
-        </div>
-        <div class="img bg-wrap text-center py-4" style="background-image: url(images/bclogin.jpg)">
-          <div class="user-logo">
-            <div> <img src="<?php echo base_url()?>images/user.png" height="100px" width="100px"></div>
-            <h3>Dosen 1</h3>
-          </div>
-        </div>
-        <ul class="list-unstyled components mb-5">
-          <li class="active">
-            <a href="<?php echo base_url() ?>admin"><span class="fa fa-home mr-3"></span> Home</a>
-          </li>
-          <li>
-            <a href="#"><span class="fa fa-support mr-3"></span> Profil</a>
-          </li>
-          <li>
-            <a href="#"><span class="fa fa-sign-out mr-3"></span> Sign Out</a>
-          </li>
-        </ul>
-      </nav>
 
-      <!-- Page Content  -->
-      <div id="content" class="p-4 p-md-5 pt-5">
-        <h2 class="mb-4">Sidebar #09</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+<div class="container">
+  <div class="row">
+    <div class="col"><br><br>
+      <h2>Mata Kuliah</h2>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <img src="<?php echo base_url(); ?>images/defaultclass.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Pemograman Web</h5>
+          <p class="card-text">Reguler Sore, Kamis 17:10 AM</p>
+          <a href="#" class="btn btn-primary">Open Class</a>
+        </div>
       </div>
     </div>
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <img src="<?php echo base_url(); ?>images/defaultclass.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Pemograman Web IOT</h5>
+          <p class="card-text">Reguler Sore, Jumat 17:10 AM</p>
+          <a href="#" class="btn btn-primary">Open Class</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -10,3 +10,52 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/style.css" />
   </head>
+
+  <body>
+    <div class="wrapper d-flex align-items-stretch">
+      <nav id="sidebar">
+        <div class="custom-menu">
+          <button type="button" id="sidebarCollapse" class="btn btn-primary"><img src="<?php echo base_url();?>images/close.png" height="25px" width="25px"></button>
+        </div>
+        <div class="img bg-wrap text-center py-4" style="background-image: url(images/bclogin.jpg)">
+          <div class="user-logo">
+            <div> <img src="<?php echo base_url()?>images/user.png" height="100px" width="100px"></div>
+            <h3>Dosen 1</h3>
+          </div>
+        </div>
+        <ul class="list-unstyled components mb-5">
+          <li class="active">
+            <a href="<?php echo base_url() ?>admin"><span class="fa fa-home mr-3"></span> Home</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-support mr-3"></span> Profil</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-sign-out mr-3"></span> Sign Out</a>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+          <li>
+            <br>
+          </li>
+        </ul>
+      </nav>
